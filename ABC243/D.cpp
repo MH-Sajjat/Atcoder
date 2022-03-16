bebe4aed8bf6937b710337db6d16d71c683a362c
MH-Sajjat <78714514+MH-Sajjat@users.noreply.github.com>
2022-03-16 09:36:44 +0600

    Create D.cpp

---
+++ ABC243/D.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long           ll;
+typedef long double         ld;
+typedef unsigned long long  ull;
+typedef pair <int, int>     pii;
+typedef pair <ll, ll>       pll;
+typedef vector <int>        vi;
+typedef vector <ll>         vll;
+
+const int N       = (int) 2e5 + 5;
+const int mxN     = (int) 1e6 + 6;
+const int MOD     = (int) 1e9 + 7;
+const int INF     = (int) 1e9 + 9;
+const double EPS  = (double) 1e-9;
+
+#define debug(x)  cerr << #x << " = " << x << '\n';
+#define all(x)    x.begin(), x.end()
+#define szof(x)   (int) x.size()
+#define ff        first
+#define ss        second
+#define pb        push_back
+#define mp        make_pair
+#define PI        acos(-1.0)
+#define nl        '\n'
+#define Fast_IO   ios_base::sync_with_stdio(false); cin.tie(nullptr);
+
+/*........................ let's try one more time ........................*/
+
+int main() {
+#ifdef LOCAL
+  freopen("input.txt", "r", stdin);
+  // freopen("output.txt", "w", stdout);
+#endif
+  Fast_IO
+  int test = 1, tc = 0;
+  while (test--) {
+    int n;
+    ll pos;
+    string s;
+    cin >> n >> pos >> s;
+    stack<char> st;
+    for (auto x : s) {
+      if (st.empty()) st.push(x);
+      else {
+        if (x == 'U' and st.top() != 'U') st.pop();
+        else st.push(x);
+      }
+    }
+    vector<char> v;
+    while (!st.empty()) {
+      v.pb(st.top());
+      st.pop();
+    }
+    reverse(all(v));
+    for (auto x : v) {
+      if (x == 'U') pos /= 2;
+      else if (x == 'L') pos *= 2;
+      else {
+        pos *= 2;
+        pos++;
+      }
+    }
+
+    cout << pos << nl;
+  }
+  return 0;
+}
