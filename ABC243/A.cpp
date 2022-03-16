2c4baad66065f1e4934c98cac9da12b8344fc171
MH-Sajjat <78714514+MH-Sajjat@users.noreply.github.com>
2022-03-16 07:46:22 +0600

    Update A.cpp

--- ABC243/A.cpp
+++ ABC243/A.cpp
@@ -1,5 +1,9 @@
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
 using namespace std;
+using namespace __gnu_pbds;
 
 typedef long long           ll;
 typedef long double         ld;
@@ -8,6 +12,14 @@ typedef pair <int, int>     pii;
 typedef pair <ll, ll>       pll;
 typedef vector <int>        vi;
 typedef vector <ll>         vll;
+typedef tree <int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds_set;
+typedef tree <int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> pbds_multiset;
+
+inline int Int() { int x; scanf("%d", &x); return x; }
+inline ll Long() { ll x; scanf("%lld", &x); return x; }
+
+int dx[8] = { 0, -1, 0, 1, -1, -1, 1, 1 };
+int dy[8] = { -1, 0, 1, 0, -1, 1, 1, -1 };
 
 const int N       = (int) 2e5 + 5;
 const int mxN     = (int) 1e6 + 6;
@@ -23,46 +35,34 @@ const double EPS  = (double) 1e-9;
 #define pb        push_back
 #define mp        make_pair
 #define PI        acos(-1.0)
+#define TN        typename
 #define nl        '\n'
 #define Fast_IO   ios_base::sync_with_stdio(false); cin.tie(nullptr);
+
+inline int add(int a, int b, int mod) { a += b; return a >= mod ? a - mod : a < 0 ? a + mod : a; }
+inline int mult(int a, int b, int mod) { return (ll) a * b % mod; }
+
+template <TN T> inline void umin(T &x, T y) { x = x < y ? x : y; }
+template <TN T> inline void umax(T &x, T y) { x = x > y ? x : y; }
+template <TN T> T gcd(T a, T b) { return !b ? a : gcd(b, a % b); }
+template <TN T> T lcm(T a, T b) { return a * (b / gcd(a, b)); }
 
 /*........................ let's try one more time ........................*/
 
 int main() {
 #ifdef LOCAL
-  freopen("input.txt", "r", stdin);
-  // freopen("output.txt", "w", stdout);
+    freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
 #endif
-  Fast_IO
-  int test = 1, tc = 0;
-  while (test--) {
-    int n;
-    cin >> n;
-    vector<pair<int, int>> v;
-    for (int i = 1; i <= n; ++i) {
-      int x, y;
-      cin >> x >> y;
-      v.pb({x, y});
+    // Fast_IO
+    int test = 1, tc = 0;
+    while (test--) {
+        int v = Int(), a = Int(), b = Int(), c = Int();
+        int k = a + b + c;
+        v %= k;
+        if ((v < a)) printf("F\n");
+        else if (v < (a + b)) printf("M\n");
+        else printf("T\n");
     }
-    string s;
-    cin >> s;
-    map<int, bool> m;
-    bool isCollision = 0;
-    vector<pair<int, pair<int, char>>> dt;
-    for (int i = 0; i < n; ++i) {
-      dt.pb({v[i].ff, {v[i].ss, s[i]}});
-    }
-    sort(all(dt));
-
-    for (int i = 0; i < n; ++i) {
-      if (dt[i].ss.ss == 'R') {
-        m[dt[i].ss.ff] = 1;
-      } else {
-        if (m[dt[i].ss.ff]) isCollision = 1;
-      }
-    }
-    if (isCollision) cout << "Yes" << nl;
-    else cout << "No" << nl;
-  }
-  return 0;
+    return 0;
 }
