0697372ea79250341c317d34fd523c6c64cbc6b4
MH-Sajjat <78714514+MH-Sajjat@users.noreply.github.com>
2022-03-29 09:23:04 +0600

    Create C.cpp

---
+++ ABC236/C.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long           ll;
+typedef long double         ld;
+typedef unsigned long long  ull;
+typedef pair <int, int>     pii;
+typedef pair <ll, ll>       pll;
+typedef vector <int>        vi;
+typedef vector <ll>         vll;
+
+const int N       = (int) 2e5 + 5;
+const int mxN     = (int) 1e6 + 6;
+const int MOD     = (int) 1e9 + 7;
+const int INF     = (int) 1e9 + 9;
+const double EPS  = (double) 1e-9;
+
+#define debug(x)  cerr << #x << " = " << x << '\n';
+#define all(x)    x.begin(), x.end()
+#define szof(x)   (int) x.size()
+#define ff        first
+#define ss        second
+#define pb        push_back
+#define mp        make_pair
+#define PI        acos(-1.0)
+#define nl        '\n'
+#define Fast_IO   ios_base::sync_with_stdio(false); cin.tie(nullptr);
+
+/*........................ let's try one more time ........................*/
+
+int main() {
+#ifdef LOCAL
+  freopen("input.txt", "r", stdin);
+  // freopen("output.txt", "w", stdout);
+#endif
+  Fast_IO
+  int test = 1, tc = 0;
+  while (test--) {
+    int n, k;
+    cin >> n >> k;
+    vector<string> v1(n), v2(k);
+    for (auto& x : v1) cin >> x;
+    for (auto& x : v2) {
+      cin >> x;
+    }
+    sort(all(v2));
+    for (auto x : v1) {
+      auto it = lower_bound(all(v2), x);
+      if (it != v2.end() and *it == x) cout << "Yes" << nl;
+      else cout << "No" << nl;
+    }
+  }
+  return 0;
+}
