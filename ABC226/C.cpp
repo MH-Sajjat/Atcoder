0ab6c673761ca7daedb880c3cae7f17c7a826a9b
MH-Sajjat <78714514+MH-Sajjat@users.noreply.github.com>
2022-04-11 10:25:34 +0600

    Create C.cpp

---
+++ ABC226/C.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long           ll;
+typedef long double         ld;
+typedef unsigned long long  ull;
+typedef pair <int, int>     pii;
+typedef pair <ll, ll>       pll;
+typedef vector <int>        vi;
+typedef vector <ll>         vll;
+
+const int N       = (int) 2e5 + 5;
+const int mxN     = (int) 1e6 + 6;
+const int MOD     = (int) 1e9 + 7;
+const int INF     = (int) 1e9 + 9;
+const double EPS  = (double) 1e-9;
+
+#define debug(x)  cerr << #x << " = " << x << '\n';
+#define all(x)    x.begin(), x.end()
+#define szof(x)   (int) x.size()
+#define ff        first
+#define ss        second
+#define pb        push_back
+#define mp        make_pair
+#define PI        acos(-1.0)
+#define nl        '\n'
+#define Fast_IO   ios_base::sync_with_stdio(false); cin.tie(nullptr);
+
+/*........................ let's try one more time ........................*/
+
+int main() {
+#ifdef LOCAL
+  freopen("input.txt", "r", stdin);
+  // freopen("output.txt", "w", stdout);
+#endif
+  Fast_IO
+  int test = 1, tc = 0;
+  while (test--) {
+    int n;
+    cin >> n;
+    vector<vi> v(n + 2);
+    vi t(n);
+    for (int i = 1; i <= n; ++i) {
+      cin >> t[i];
+      int k;
+      cin >> k;
+      for (int j = 1; j <= k; ++j) {
+        int x;
+        cin >> x;
+        v[i].pb(x);
+      }
+    }
+    ll mn = 0;
+    priority_queue<int> pq;
+    vi vis(n + 2, 0);
+    pq.push(n);
+    while (!pq.empty()) {
+      int id = pq.top();
+      pq.pop();
+      if (vis[id]) continue;
+      mn += t[id];
+      vis[id] = 1;
+      for (auto x : v[id]) pq.push(x);
+    }
+
+    cout << mn << nl;
+  }
+  return 0;
+}
