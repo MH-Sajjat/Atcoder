c198757b60285385e58a34905202f75cf4b83b06
MH-Sajjat <78714514+MH-Sajjat@users.noreply.github.com>
2022-04-13 09:25:17 +0600

    Create E.cpp

---
+++ ABC231/E.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long           ll;
+typedef long double         ld;
+typedef unsigned long long  ull;
+typedef pair <int, int>     pii;
+typedef pair <ll, ll>       pll;
+typedef vector <int>        vi;
+typedef vector <ll>         vll;
+
+const int N       = (int) 2e5 + 5;
+const int mxN     = (int) 1e6 + 6;
+const int MOD     = (int) 1e9 + 7;
+const int INF     = (int) 1e9 + 9;
+const double EPS  = (double) 1e-9;
+
+#define debug(x)  cerr << #x << " = " << x << '\n';
+#define all(x)    x.begin(), x.end()
+#define szof(x)   (int) x.size()
+#define ff        first
+#define ss        second
+#define pb        push_back
+#define mp        make_pair
+#define PI        acos(-1.0)
+#define nl        '\n'
+#define Fast_IO   ios_base::sync_with_stdio(false); cin.tie(nullptr);
+
+/*........................ let's try one more time ........................*/
+
+vll coin(100, 0);
+map<pair<int, ll>, ll> ma;
+
+
+ll call(int n, ll sum) {
+  if (n == 1) return sum;
+  if (sum == 0) return 0;
+  if (ma[{n, sum}]) return ma[{n, sum}];
+  ma[{n, sum}] = call(n - 1, sum % coin[n]) + sum / coin[n];
+  if (sum % coin[n]) ma[{n, sum}] = min(ma[{n, sum}], call(n - 1, coin[n] - sum % coin[n]) + sum / coin[n] + 1);
+  return ma[{n, sum}];
+}
+
+int main() {
+#ifdef LOCAL
+  freopen("input.txt", "r", stdin);
+  // freopen("output.txt", "w", stdout);
+#endif
+  Fast_IO
+  int test = 1, tc = 0;
+  while (test--) {
+    int n;
+    ll x;
+    cin >> n >> x;
+    for (int i = 1; i <= n; ++i) cin >> coin[i];
+    cout << call(n, x) << nl;
+  }
+  return 0;
+}
