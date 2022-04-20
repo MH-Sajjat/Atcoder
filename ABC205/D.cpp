d5ff92b5df912299cf231588c8ec6d13eaec547c
MH-Sajjat <78714514+MH-Sajjat@users.noreply.github.com>
2022-04-20 11:24:38 +0600

    Create D.cpp

---
+++ ABC205/D.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long           ll;
+typedef long double         ld;
+typedef unsigned long long  ull;
+typedef pair <int, int>     pii;
+typedef pair <ll, ll>       pll;
+typedef vector <int>        vi;
+typedef vector <ll>         vll;
+
+const int N       = (int) 2e5 + 5;
+const int mxN     = (int) 1e6 + 6;
+const int MOD     = (int) 1e9 + 7;
+const int INF     = (int) 1e9 + 9;
+const double EPS  = (double) 1e-9;
+
+#define debug(x)  cerr << #x << " = " << x << '\n';
+#define all(x)    x.begin(), x.end()
+#define szof(x)   (int) x.size()
+#define ff        first
+#define ss        second
+#define pb        push_back
+#define mp        make_pair
+#define PI        acos(-1.0)
+#define nl        '\n'
+#define Fast_IO   ios_base::sync_with_stdio(false); cin.tie(nullptr);
+
+/*........................ let's try one more time ........................*/
+
+int main() {
+#ifdef LOCAL
+  freopen("input.txt", "r", stdin);
+  // freopen("output.txt", "w", stdout);
+#endif
+  Fast_IO
+  int test = 1, tc = 0;
+  while (test--) {
+    int n, q;
+    cin >> n >> q;
+    vll v(n, 0);
+    for (auto& x : v) cin >> x;
+    while (q--) {
+      ll l = 1, r;
+      cin >> r;
+      while (1) {
+        auto it = lower_bound(all(v), l);
+        auto it1 = upper_bound(all(v), r);
+        ll dist = it1 - it;
+        if (dist == 0) break;
+        l = r + 1;
+        r = r + dist;
+      }
+      cout << r << nl;
+    }
+  }
+  return 0;
+}
